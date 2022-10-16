643c0feb11c956e104c512b781f845b1063756ec
elliot-lyons <elliot243lyons@gmail.com>
2022-10-16 20:05:39 +0100

    Most of part one done. Just end to finish and tidy up

--- Exercise02/src/Ex02.hs
+++ Exercise02/src/Ex02.hs
@@ -1,10 +1,12 @@
 {- butrfeld Andrew Butterfield -}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Ex02 where
+import Data.Version (Version(Version))
 
 name, idno, username :: String
-name      =  "Myself, Me"  -- replace with your name
-idno      =  "01234567"    -- replace with your student id
-username  =  "memyselfi"   -- replace with your TCD username
+name      =  "Lyons, Elliot"  -- replace with your name
+idno      =  "20333366"    -- replace with your student id
+username  =  "lyonse7"   -- replace with your TCD username
 
 
 declaration -- do not modify this
@@ -57,7 +59,46 @@ v42 = Val 42 ; j42 = Just v42
   -- see test outcomes for the precise format of those messages
 
 eval :: EDict -> Expr -> Either String Double
-eval d e = error "eval NYI"
+
+eval d (Val x) =  Right x
+
+eval d (Var e) = find d e
+
+eval d (Add x y) = add (eval d x)  (eval d y)         -- could do one function for all?
+eval d (Sub x y) = sub (eval d x)  (eval d y)
+eval d (Mul x y) = mul (eval d x)  (eval d y)
+eval d (Dvd x y) = dvd (eval d x)  (eval d y)
+eval _ (Def e z k) = Right 55.1
+         
+
+add :: Either String Double -> Either String Double -> Either String Double
+
+add (Left x) _ =  Left x
+add (_) (Left y) = Left y
+
+add (Right x) (Right y) = Right (x + y)
+
+sub :: Either String Double -> Either String Double -> Either String Double
+
+sub (Left x) _ =  Left x
+sub (_) (Left y) = Left y
+
+sub (Right x) (Right y) = Right (x - y)
+
+mul :: Either String Double -> Either String Double -> Either String Double
+
+mul (Left x) _ =  Left x
+mul (_) (Left y) = Left y
+
+mul (Right x) (Right y) = Right (x * y)
+
+dvd :: Either String Double -> Either String Double -> Either String Double
+
+dvd (Left x) _ =  Left x
+dvd (_) (Left y) = Left y
+
+dvd (Right x) (Right y) | y == 0 = Left "div by zero"
+                        | otherwise = Right  (x / y)
 
 -- Part 2 : Expression Laws -- (15 test marks, worth 15 Exercise Marks) --------
 
